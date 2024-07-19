f1c8cb2c3c760ac1e894db23ce7ab1410e5a14e0
Nikita Mikryukov <97m.nik@gmail.com>
2024-07-19 10:28:06 +0300

    Initial Commit

---
+++ ActivityLiveWords/ActivityLiveWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityLiveWordsApp.swift
+//  ActivityLiveWords
+//
+//  Created by Nikita on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ActivityLiveWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
